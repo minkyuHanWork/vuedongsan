96dc9f94db532225bd9a27daf59b828e899ee4cd
minkyuHanWork <minkyu.han.work@gmail.com>
2023-03-12 17:36:37 +0900

    kakaomap

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <OneRoomDetail
+  <!-- <OneRoomDetail
     @closeModal="모달창열렸니 = false"
     :원룸들="원룸들"
     :모달창열렸니="모달창열렸니"
     :원룸번호="원룸번호"
-  />
+  /> -->
 
   <div class="menu">
     <a v-for="(a, i) in menu_names" :key="i">{{ a }}</a>
@@ -14,14 +14,14 @@
     <v-text-field placeholder="우편번호" v-model="zonecode" readonly />
     <button id="postcode" @click="openPostcode">검색</button><br />
     <v-text-field placeholder="주소" v-model="roadAddress" readonly /><br />
-    <v-text-fieldㅊ placeholder="상세주소" v-model="detailAddress" /><br />
+    <v-text-field placeholder="상세주소" v-model="detailAddress" /><br />
   </div>
 
-  <MyDiscount></MyDiscount>
+  <!-- <MyDiscount></MyDiscount> -->
 
   <AddressExample></AddressExample>
 
-  <div v-for="(원룸, i) in 원룸들" :key="i">
+  <!-- <div v-for="(원룸, i) in 원룸들" :key="i">
     <OneRoomList
       @openModal="
         모달창열렸니 = true;
@@ -31,7 +31,7 @@
       :모달창열렸니="모달창열렸니"
       :원룸번호="원룸번호"
     />
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -59,9 +59,16 @@ export default {
     openPostcode() {
       new window.daum.Postcode({
         oncomplete: (data) => {
-          console.log(data);
           this.zonecode = data.zonecode;
           this.roadAddress = data.roadAddress;
+          var geocoder = new daum.maps.services.Geocoder();
+          geocoder.addressSearch(data.address, function (results, status) {
+            if (status === daum.maps.services.Status.OK) {
+              var result = results[0];
+              var coords = new daum.maps.LatLng(result.y, result.x);
+              console.log(coords);
+            }
+          });
         },
       }).open();
     },
